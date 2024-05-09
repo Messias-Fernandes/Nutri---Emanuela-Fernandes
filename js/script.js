7056c0e5c23f8686a46e8283cb2df2e81d54dca9
Messias Fernandes <fr.messias27@gmail.com>
2024-05-08 23:50:21 -0300

    daclarando variaveis

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('lets go')
+
+var nome = 'Messias',
+idade = 23;
+
+
+ var comidaFavorita;
+
+ comidaFavorita = 'Carne'
+
+var fezFaculdade = true,
+    nomeDoPet = 'coxinha' ,
+    time = 'Flamengo' ,
+    diaDoAniversario = 27,
+    hobby = 'jogar bola';
+
+
+console.log(nome, idade, comidaFavorita, time)
