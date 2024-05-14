6c5634b42e86b3ced1d678b1aae60aa18f3c032f
Messias Fernandes <fr.messias27@gmail.com>
2024-05-13 23:46:25 -0300

    arrays e loops

--- js/script.js
+++ js/script.js
@@ -46,3 +46,13 @@ var unidade = 'kg';
 var peso = numero + unidade; // '80kg'
 
 console.log(peso);
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for (var i = 0; i < brasilCampeao.length; i++) {
+  console.log(`O brasil ganhou a copa de ${brasilCampeao[i]}`);
+}
